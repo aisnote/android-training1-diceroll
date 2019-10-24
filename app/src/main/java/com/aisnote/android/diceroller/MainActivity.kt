53eaf9ac510ec8dbe9eb9cdd648332e94bf29b01
aisnote <aisnote@gmail.com>
2019-10-23 21:44:25 -0700

    first class on android

---
+++ app/src/main/java/com/aisnote/android/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.aisnote.android.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{ rollDive() }
+
+        var countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener{ countUp() }
+    }
+
+    private fun countUp() {
+        var resultText: TextView = findViewById(R.id.result_text)
+        var result = resultText.text.toString()
+        if (result == "Hello World") {
+            resultText.text = "1"
+        } else if (result == "6") {
+
+        } else {
+            var temp = result.toInt() + 1
+            resultText.text = temp.toString()
+        }
+    }
+
+    private fun rollDive() {
+//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+        var resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+}
